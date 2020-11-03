135a3306ca5dd601630f642db6e27de772d9e7ba
marcelcruz85 <marcelcruz85@gmail.com>
2020-11-03 16:08:53 -0500

    Auth Completed

--- src/app/user/reset/reset.page.ts
+++ src/app/user/reset/reset.page.ts
@@ -1,3 +1,4 @@
+import { AlertService } from './../../_services/alert.service';
 import { AuthService } from './../../_services/auth.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
@@ -14,26 +15,34 @@ export class ResetPage implements OnInit {
 
   constructor(
     private auth: AuthService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private alert: AlertService
   ) { }
 
   ngOnInit() {
     this.route.queryParams.subscribe( params => {
       if ( params ) {
         this.code = params.code;
-        // console.log(params);
-        // const queryParams = JSON.parse(params);
-        // console.log(queryParams);
+        console.log(this.code);
       }
     });
   }
 
-  async reset(){
-    this.auth.reset(this.password, this.passwordConfirm, this.code).subscribe(res => {
-      console.log(res);
-    }, (err) => {
-      console.log(err);
-    });
+  reset(){
+    if (this.password && this.passwordConfirm) {
+      if (this.password === this.passwordConfirm) {
+        this.auth.reset(this.password, this.passwordConfirm, this.code).subscribe( res => {
+          this.alert.showAlert('Your password have been reset');
+        }, (err) => {
+          this.alert.showAlert(err.error.message[0].messages[0].message);
+          console.log(err);
+        });
+      }else{
+        this.alert.showAlert('Password doesn\'t match');
+      }
+    } else {
+      this.alert.showAlert('All fields are required');
+    }
   }
 
 }
